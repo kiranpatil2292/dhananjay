0b2f265a5d5f597749776eb2a7556eaac0403593
kiran <patilkiran.1@yahoo.com>
2024-12-30 11:30:40 +0530

    kiran.py file is commited

--- testsimpleBookApi/test_kp.py
+++ testsimpleBookApi/test_kp.py
@@ -10,10 +10,12 @@ print('test execution completed')
 
 
 def test_sum_of_ele(setup):
-    value = setup1
+    value = setup2
     assert value == 12
 
 
 def test_tsum_of_ele1(setup):
-    value = setup2
+    value = setup1
     assert value == 13, "required value should be 12"
+#kiran patil
+i=20
